0497ae93c8c8ab80f9c304786426765275864d48
yangziongheng <binguodexin@163.com>
2017-09-13 15:07:58 +0800

    test2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <div class="nav-top">
+        <!-- 引入公用的头部 header组件 -->
+          <v-header></v-header>
+    </div>
+    <div class="banner">
+    </div>
+    <div class="contianer">
+      <!-- 路由中的几个组件在这里被渲染，默认被渲染的为第一个组件，也就是home组件  -->
+      <router-view></router-view>
+    </div>
+    <div class="footer">
+      <!-- 引入公用的底部 footer组件 -->
+      <v-footer></v-footer>
+    </div>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'app'
-}
-</script>
-
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -18,6 +23,29 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.nav-top {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  margin-left: -600px;
+  z-index: 99;
+}
+.banner{
+  width: 100%;
+  height: 370px;
+  overflow: hidden;
+  background: url("assets/banner.jpg");
+  background-repeat: no-repeat;
 }
 </style>
+<script>
+import header from './components/header.vue'
+import footer from './components/footer.vue'
+export default{
+  components: {
+    'v-header': header,
+    'v-footer': footer
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,43 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import Router from 'vue-router'  // 引入router路由
+
+// 引入项目的四个模块组件
+import introduce from './components/introduce'
+import home from './components/home'
+import employment from './components/employment'
+import consult from './components/consult'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+Vue.use(Router) // 使用router
+
+// 定义路由
+var routes = [{
+  path: '/home',
+  component: home
+}, {
+  path: '/introduce',
+  component: introduce
+}, {
+  path: '/employment',
+  component: employment
+}, {
+  path: '/consult',
+  component: consult
+}]
+
+// 实例化路由
+var vueRouter = new Router({
+  routes
+})
+
 new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
+  router: vueRouter,
+  template: '<pp><App/>',
   components: { App }
 })
